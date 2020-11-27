41df84fb9cc58291edfd982e8c508316de80710b
Paolo Trilli <paolo.trilli@mail.polimi.it>
2020-11-27 17:44:20 +0100

    gym env changes

--- src/model/gym_prova.py
+++ src/model/gym_prova.py
@@ -1,14 +1,14 @@
 import gym
 from src.model.env2048 import Env2048
 
-env = Env2048()
+env = gym.make('Pendulum-v0')
 
-for i_episode in range(3):
+for i_episode in range(20):
     observation = env.reset()
-    for t in range(3):
+    for t in range(100):
         env.render()
-
-        action = 3
+        print(observation)
+        action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
